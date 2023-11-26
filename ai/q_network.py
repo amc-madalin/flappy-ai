6d7f0a939c94fbea6098547eac7cf4e0d641fc20
amcostea <amc.madalin@gmail.com>
2023-11-26 10:49:54 +0200

    Replace ReLU with LeakyReLU for negative inputs

--- ai/q_network.py
+++ ai/q_network.py
@@ -1,23 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-
-# Q-Network definition
 class QNetwork(nn.Module):
     def __init__(self):
         super(QNetwork, self).__init__()
         # Define network layers
-        self.fc1 = nn.Linear(3, 64)  # First fully connected layer
+        self.fc1 = nn.Linear(4, 64)  # First fully connected layer
         self.fc2 = nn.Linear(64, 64)  # Second fully connected layer
         self.fc3 = nn.Linear(64, 32)  # Third fully connected layer
         self.fc4 = nn.Linear(32, 2)   # Output layer
 
+        # Define LeakyReLU activation
+        self.leaky_relu = nn.LeakyReLU()
+
     def forward(self, x):
-        # Forward pass through the network
-        x = torch.relu(self.fc1(x))  # Activation function after first layer
-        x = torch.relu(self.fc2(x))  # Activation function after second layer
-        x = torch.relu(self.fc3(x))  # Activation function after third layer
-        x = self.fc4(x)              # No activation function after output layer
+        # Forward pass through the network using LeakyReLU
+        x = self.leaky_relu(self.fc1(x))  # Activation function after first layer
+        x = self.leaky_relu(self.fc2(x))  # Activation function after second layer
+        x = self.leaky_relu(self.fc3(x))  # Activation function after third layer
+        x = self.fc4(x)                   # No activation function after output layer
         return x
 
 def init_weights(m):
