88ae607357317c6369931eb5dca3e0824ffe24ad
amc-madalin <amc.madalin@gmail.com>
2023-12-08 14:14:02 +0200

    Fix game to 60FPS

--- test_game.py
+++ test_game.py
@@ -22,33 +22,51 @@ def test_game(model_path, config):
     # Initialize game variables
     chr_x, chr_y, pipe_height, pipe_x, pipe_speed, _, _, _, _, _ = init_game_variables(config)
     
+    # Constants
+    FPS = 60  # Desired number of updates per second
+    TIME_PER_UPDATE = 1.0 / FPS  # Time per game logic update
+
+    # Initialize clock
+    clock = pygame.time.Clock()
+    accumulator = 0.0
+    
     running = True
     while running:
-        # Handle events like game exit
+        # Calculate elapsed time
+        delta_time = clock.tick(FPS) / 1000.0  # Convert milliseconds to seconds
+        accumulator += delta_time
+
+        # Handle events
         running = handle_events()
 
-        # Get the current state representation
-        state = get_state_representation(chr_x, chr_y, pipe_x, pipe_height, config)
+        # Update game logic at fixed intervals
+        if accumulator >= TIME_PER_UPDATE:
+            while accumulator >= TIME_PER_UPDATE:
+                state = get_state_representation(
+                    chr_x, chr_y, pipe_x, pipe_height, config
+                )
+                # Forward pass through the network to get Q-values for the current state
+                with torch.no_grad():  # We do not need gradient computation for testing
+                    q_values = q_network(state)
+                
+                # Select the action with the highest Q-value
+                action = torch.argmax(q_values).item()
 
-        # Forward pass through the network to get Q-values for the current state
-        with torch.no_grad():  # We do not need gradient computation for testing
-            q_values = q_network(state)
-        
-        # Select the action with the highest Q-value
-        action = torch.argmax(q_values).item()
+                # Update the game state based on the selected action
+                chr_y, pipe_x, pipe_height = update_game_state(action, chr_y, pipe_x, pipe_height, config)
 
-        # Update the game state based on the selected action
-        chr_y, pipe_x, pipe_height = update_game_state(action, chr_y, pipe_x, pipe_height, config)
+                accumulator -= TIME_PER_UPDATE
 
-        # Render the game screen
-        render_game_screen(screen, chr_x, chr_y, pipe_x, pipe_height, config)
+            # Render the game screen
+            render_game_screen(screen, chr_x, chr_y, pipe_x, pipe_height, config)
+            pygame.display.flip()  # Update the display
 
-        # Check for collision and update reward (for testing, we may not need the reward)
-        _, _, done = check_collision_and_update_reward(chr_x, chr_y, pipe_x, pipe_height, config)
+            # Check for collision and update reward (for testing, we may not need the reward)
+            _, _, done = check_collision_and_update_reward(chr_x, chr_y, pipe_x, pipe_height, config)
 
-        if done:
-            # If the game is over, exit the loop
-            break
+            if done:
+                # If the game is over, exit the loop
+                break
 
     pygame.quit()
 
