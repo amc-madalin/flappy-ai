6903610f1147a4aaa0e6ef74692cd4e59c604be8
amc-madalin <amc.madalin@gmail.com>
2023-12-08 12:57:44 +0200

    Add tensorboard and save best model

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from ai.q_network import init_q_network
 from ai.training import update_q_values
 from utils.utils import load_config
 
+import os
 import torch
 import random
 import pygame
+
+from torch.utils.tensorboard import SummaryWriter
 
 def main():
     # Load configuration
@@ -21,9 +24,14 @@ def main():
     save_path = config['model']['save_path']
     replay_buffer = []
 
+    # Initialize TensorBoard writer
+    writer = SummaryWriter('runs/flappy_bird_experiment')
+
     for episode in range(num_episodes):
         chr_x, chr_y, pipe_height, pipe_x, pipe_speed, actions, epsilon, epsilon_min, epsilon_decay, reward = init_game_variables(config)
         running = True
+        best_reward = 0
+        total_reward = 0  # Initialize total reward for the episode
 
         while running:
             running = handle_events()
@@ -39,6 +47,11 @@ def main():
             chr_y, pipe_x, pipe_height = update_game_state(action, chr_y, pipe_x, pipe_height, config)
             render_game_screen(screen, chr_x, chr_y, pipe_x, pipe_height, config)
             new_state, reward, done = check_collision_and_update_reward(chr_x, chr_y, pipe_x, pipe_height, config)
+            
+            total_reward += reward
+
+            # Log episode reward using TensorBoard
+            writer.add_scalar('Reward/Episode', total_reward, episode)
 
             experience = (torch.tensor(state, dtype=torch.float), action, reward, torch.tensor(new_state, dtype=torch.float))
             replay_buffer.append(experience)
@@ -50,19 +63,27 @@ def main():
 
             if done:
                 break
-        model_save_path = save_path + str(episode + 1) + ".pth"
-        # Save the model and training state
-        torch.save({
-            'episode': episode + 1,
-            'model_state_dict': q_network.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'replay_buffer': replay_buffer
-        }, model_save_path)
+        
+        # After the episode ends, check if this episode's reward is the best
+        if total_reward > best_reward:
+            best_reward = total_reward
+            # Define the best model save path
+            best_model_save_path = os.path.join(save_path, f"best_model_reward_{total_reward}_episode_{episode + 1}.pth")
+            # Save the best model
+            torch.save({
+                'episode': episode + 1,
+                'model_state_dict': q_network.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'replay_buffer': replay_buffer,
+                'total_reward': best_reward
+            }, best_model_save_path)
+            print(f"New best model saved with reward: {total_reward} at episode {episode + 1}")
         
         print(f"Episode {episode + 1} completed. Epsilon: {epsilon}")
-
-
+        # Optionally log other metrics such as epsilon
+        writer.add_scalar('Epsilon/Episode', epsilon, episode)
     
+    writer.close()
     pygame.quit()
 
 if __name__ == "__main__":
